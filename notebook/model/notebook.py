8ad6e78b7ef6112cf96f3ec6ef7a5ba50a4b31c2
maicolpq31 <kevincanizales07@gmail.com>
2024-04-03 13:51:15 -0500

    first commit

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,1 +1,38 @@
+from dataclasses import dataclass
 from datetime import datetime
+
+@dataclass
+class Note:
+
+    creation_time: datetime = field(default_factory = 2024, 4, 3)
+
+
+
+    tags: list[str] = field(default_factory = list)
+
+
+    def __str__(self):
+        return f"code {code} n/ creation date: {creation date} n/ title {text}"
+
+
+    def add_tag(self):
+
+        pass
+
+
+    def add_note(self):
+        pass
+
+
+    def important_notes(self):
+        pass
+
+
+class NoteBook:
+
+
+    def tags_note_count(self, ):
+
+
+
+
