20386800a81b21d9530d041eadcfda401fc1ae5f
michaelpq31 <pardo.maicol.mp@gmail.com>
2024-04-04 08:11:29 -0500

    second commit

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List, Dict
 from datetime import datetime
-
 
 @dataclass
 class Note:
@@ -12,35 +10,40 @@ class Note:
     code: int
     title: str
     text: str
-    #creation_time: datetime = field(default_factory= datetime.now)
+    #creation_date: datetime = field(default_factory=datetime.now)
     importance: str
-    tags: List[str] = field(default_factory=list)
+    tags: list[str] = field(default_factory=list)
 
-    def add_tag(self, tag: str) -> None:
+    def add_tag(self, tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
 
-    def _str_(self) -> str:
+
+    def __str__(self) -> str:
         return f"Code: {self.code}\nCreation date: {self.creation_date}\n{self.title}: {self.text}"
 
 
 class Notebook:
-    def _init_(self):
-        self.notes: List[Note] = []
-        self.next_code: int = 1
+    def __init__(self):
+        self.notes: dict[int, Note] = {}
+
 
     def add_note(self, title: str, text: str, importance: str) -> int:
-        note = Note(code=self.next_code, title=title, text=text, importance=importance)
-        self.notes.append(note)
-        self.next_code += 1
-        return note.code
+        code = len(self.notes) + 1
+        self.notes[code] = Note(code=code, title=title, text=text, importance=importance)
+        return code
 
-    def important_notes(self) -> List[Note]:
-        return [note for note in self.notes if note.importance in (Note.HIGH, Note.MEDIUM)]
+    def important_notes(self) -> list[Note]:
+        important = []
+        for note in self.notes.values():
+            if note.importance.upper() in [Note.HIGH, Note.MEDIUM]:
+                important.append(note)
+        return important
 
-    def tags_note_count(self) -> Dict[str, int]:
-        tag_count: Dict[str, int] = {}
-        for note in self.notes:
+
+    def tags_note_count(self) -> dict[str, int]:
+        tag_count: dict[str, int] = {}
+        for note in self.notes.values():
             for tag in note.tags:
                 tag_count[tag] = tag_count.get(tag, 0) + 1
         return tag_count
